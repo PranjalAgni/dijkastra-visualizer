cea131d9332c3af59bc3ea9d90b77f9ffd513522
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2018-05-02 23:41:11 +0530

    make it better

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.listen(8080 , function() {
-	console.log('App started at port 8080');
-});
+
+
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
